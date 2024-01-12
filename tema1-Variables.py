3439f1b7079ee8df829d13656bc302f571685621
MarioVargas-C <mario.vargasbuss@gmail.com>
2024-01-11 21:09:16 -0600

    Repaso Python

---
+++ tema1-Variables.py
@@ -0,0 +1,25 @@
+#Nombre de variables
+
+''' 
+comentarios largos 
+
+'''
+
+numero1 = "123"
+numero2 = "10"
+nombre  = "Mario"
+correo  = "mario@gmail.com"
+
+print(numero1 + numero2)
+
+#Conversion forzada entre tipos de datos 
+
+print(int(numero1) + int(numero2))
+
+sueldo= "123.203"
+valorDecimal= float(sueldo)
+
+print(valorDecimal)
+
+edad= 23
+print(len(str(edad)))
